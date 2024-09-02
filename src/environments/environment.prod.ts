be8ac2c0cbb5f76af648b1417a066289f5b1c42c
Leon Hochwimmer <leon.hochwimmer@coop.ch>
2024-09-02 10:33:35 +0200

    Add envrionments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://mwpgjziytsvtlmiuanmo.supabase.co',
+  supabaseClient:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im13cGdqeml5dHN2dGxtaXVhbm1vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUyNjI5NzgsImV4cCI6MjA0MDgzODk3OH0.2Og26oeTDBsbcMyR99mVTb3e-nCKFkWWd9rJxmpKpiw',
+};
