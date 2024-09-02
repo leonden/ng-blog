47bd524e0c15bc7fe04d02366438bf4f9386fd0f
Leon Hochwimmer <leon.hochwimmer@coop.ch>
2024-09-02 10:51:42 +0200

    Add daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        default: {
+          primary: "#60a5fa",
+          secondary: "#c7d2fe",
+          accent: "#e9d5ff",
+          neutral: "#f3f4f6",
+          "base-100": "#ffffff",
+          info: "#7dd3fc",
+          success: "#6ee7b7",
+          warning: "#fcd34d",
+          error: "#f87171",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 };
